e698764732a01d70b55d0d25b6fbc71839607591
Tobias Raatiniemi <raatiniemi@gmail.com>
2020-12-05 21:26:04 +0100

    improve tests for day one solutions

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,70 +1,84 @@
 const EXPECTED_SUM: i32 = 2020;
 
-fn calculate_bonus_answer(input: Vec<i32>) -> Option<i32> {
-    let mut value: Option<i32> = None;
+fn calculate_part_one(input: Vec<i32>) -> Option<i32> {
+    for lhs in &input {
+        for rhs in &input {
+            let sum = lhs + rhs;
+            if sum == EXPECTED_SUM {
+                return Some(lhs * rhs);
+            }
+        }
+    }
+    None
+}
 
-    'main: for first in &input {
+fn calculate_part_two(input: Vec<i32>) -> Option<i32> {
+    for first in &input {
         for second in &input {
             for third in &input {
                 let sum = first + second + third;
-                if sum != EXPECTED_SUM {
-                    continue;
+                if sum == EXPECTED_SUM {
+                    return Some(first * second * third);
                 }
-
-                let answer = first * second * third;
-                println!("{} + {} + {} = 2020", first, second, third);
-                println!("{} * {} * {} = {}", first, second, third, answer);
-                value = Some(answer);
-                break 'main;
             }
         }
     }
-
-    value
-}
-
-fn calculate_standard_answer(input: Vec<i32>) -> Option<i32> {
-    let mut value: Option<i32> = None;
-
-    'main: for lhs in &input {
-        for rhs in &input {
-            let sum = lhs + rhs;
-            if sum != EXPECTED_SUM {
-                continue;
-            }
-
-            let answer = lhs * rhs;
-            println!("{} + {} = 2020", lhs, rhs);
-            println!("{} * {} = {}", lhs, rhs, answer);
-            value = Some(answer);
-            break 'main;
-        }
-    }
-
-    value
+    None
 }
 
 #[cfg(test)]
 mod tests {
+    use advent_of_code::read_contents_of_file;
+
     use super::*;
 
-    const TEST_INPUT: [i32; 6] = [1721, 979, 366, 299, 675, 1456];
-
     #[test]
-    fn test_standard_mode() {
+    fn day_one_part_one_with_example() {
+        let input: Vec<i32> = Vec::from([1721, 979, 366, 299, 675, 1456]);
         let expected: Option<i32> = Some(514579);
 
-        let actual = calculate_standard_answer(TEST_INPUT.to_vec());
+        let actual = calculate_part_one(input);
 
+        println!("Day #1 (part one) with example: {}", actual.unwrap());
         assert_eq!(expected, actual);
     }
 
     #[test]
-    fn test_bonus_mode() {
+    fn day_one_part_one_with_input() {
+        let input = read_contents_of_file("input/1")
+            .iter()
+            .map(|v| { v.parse::<i32>().unwrap() })
+            .collect();
+        let expected: Option<i32> = Some(326211);
+
+        let actual = calculate_part_one(input);
+
+        println!("Day #1 (part one) with input: {}", actual.unwrap());
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn day_one_part_two_with_example() {
+        let input: Vec<i32> = Vec::from([1721, 979, 366, 299, 675, 1456]);
         let expected: Option<i32> = Some(241861950);
 
-        let actual = calculate_bonus_answer(TEST_INPUT.to_vec());
+        let actual = calculate_part_two(input);
 
+        println!("Day #1 (part two) with example: {}", actual.unwrap());
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn day_one_part_two_with_input() {
+        let input = read_contents_of_file("input/1")
+            .iter()
+            .map(|v| { v.parse::<i32>().unwrap() })
+            .collect();
+        let expected: Option<i32> = Some(131347190);
+
+        let actual = calculate_part_two(input);
+
+        println!("Day #1 (part two) with input: {}", actual.unwrap());
         assert_eq!(expected, actual);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
+/// Read the contents of a file on a given path.
+pub fn read_contents_of_file(path: &str) -> Vec<String> {
+    return fs::read_to_string(path)
+        .expect(&format!("Unable to read file at path {}", path).as_str())
+        .split("\n")
+        .filter(|value| { !value.is_empty() })
+        .map(|value| { String::from(value) })
+        .collect();
+}
+
 /// Read character at index from String.
 ///
 /// With a valid index, the character (as [String]) at the index will be returned as [Option.Some].
