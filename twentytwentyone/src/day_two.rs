1461b87e694af6df04adaca16a32805313d68e85
Tobias Raatiniemi <raatiniemi@gmail.com>
2021-12-05 09:52:31 +0100

    prepare day two solution for part two

--- twentytwentyone/src/day_two.rs
+++ twentytwentyone/src/day_two.rs
@@ -1,22 +1,51 @@
+use Command::{Down, Forward, Up};
+
+enum Command {
+    Forward { x: i32 },
+    Down { x: i32 },
+    Up { x: i32 },
+}
+
 fn calculate_part_one(input: Vec<String>) -> Option<i32> {
+    let calculate_position_adjustment = |current_position: (i32, i32), command: Command| -> (i32, i32) {
+        let adjustment = match command {
+            Forward { x } => (x, 0),
+            Down { x } => (0, x),
+            Up { x } => (0, -x),
+        };
+
+        (current_position.0 + adjustment.0, current_position.1 + adjustment.1)
+    };
+    let initial_position = (0, 0);
     let (x, y) = input.iter()
-        .map(|v| {
-            let mut x = v.splitn(2, ' ');
-            (x.next().unwrap_or(""), x.next().unwrap_or("").parse::<i32>().unwrap_or(0))
-        })
-        .map(|(s, v)| {
-            match s {
-                "forward" => (v, 0),
-                "down" => (0, v),
-                "up" => (0, -v),
-                _ => (0, 0)
+        .map(parse_command)
+        .fold(initial_position, |current_position, value| {
+            match value {
+                Some(command) => calculate_position_adjustment(current_position, command),
+                None => current_position,
             }
-        })
-        .fold((0, 0), |acc, f| {
-            (acc.0 + f.0, acc.1 + f.1)
         });
 
     Some(x * y)
+}
+
+fn parse_command(v: &String) -> Option<Command> {
+    let mut x = v.splitn(2, ' ');
+    let name = x.next().unwrap_or("");
+    let value = x.next().unwrap_or("")
+        .parse::<i32>()
+        .unwrap_or(0);
+
+    parse_raw_command(name, value)
+}
+
+fn parse_raw_command(name: &str, value: i32) -> Option<Command> {
+    match name {
+        "forward" => Some(Forward { x: value }),
+        "down" => Some(Down { x: value }),
+        "up" => Some(Up { x: value }),
+        _ => None
+    }
 }
 
 #[cfg(test)]
