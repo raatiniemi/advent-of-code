8629f4524ba3a626d73692064c84b42108541d05
Tobias Raatiniemi <raatiniemi@gmail.com>
2022-06-28 11:20:32 +0200

    add txt suffix for 2015 input files

--- python/src/lib.py
+++ python/src/lib.py
@@ -4,7 +4,7 @@ Point = namedtuple("Point", "x y")
 
 
 def read_input(year: int, day: int, part: int = 1) -> str:
-    file = open(f"input/{year}/day_{day}_{part}", "r")
+    file = open(f"input/{year}/day_{day}_{part}.txt", "r")
     content = file.read()
     file.close()
 
