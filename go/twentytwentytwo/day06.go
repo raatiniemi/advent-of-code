3d09f3e39e4b3cd8d4b8d2ef16002d6ccb90efd9
Tobias Raatiniemi <raatiniemi@gmail.com>
2022-12-07 06:14:06 +0100

    extract shared go function for 2022 day 6 solution

--- go/twentytwentytwo/day06.go
+++ go/twentytwentytwo/day06.go
@@ -3,8 +3,13 @@ package twentytwentytwo
 import "strings"
 
 func day06Part01(input string) int {
+	return findStartOfPacketMarker(input, 4)
+}
+
+func findStartOfPacketMarker(input string, size int) int {
 	result := 0
-	for _, chars := range window[string](characters(input), 4) {
+
+	for _, chars := range window[string](characters(input), size) {
 		s := make(map[string]bool)
 		for _, char := range chars {
 			s[char] = true
@@ -14,21 +19,10 @@ func day06Part01(input string) int {
 			break
 		}
 	}
+
 	return result
 }
 
 func day06Part02(input string) int {
-	result := 0
-
-	for _, chars := range window[string](characters(input), 14) {
-		s := make(map[string]bool)
-		for _, char := range chars {
-			s[char] = true
-		}
-		if len(chars) == len(s) {
-			result = strings.Index(input, strings.Join(chars, "")) + len(s)
-			break
-		}
-	}
-	return result
+	return findStartOfPacketMarker(input, 14)
 }
